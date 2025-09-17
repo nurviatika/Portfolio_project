0bb6f8ab98dafccfe6149b398f87cdc1257c7bd9
siti nurviatika <sitinurviatika@gmail.com>
2025-09-17 21:15:50 +0700

    revisi file vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "src/aboutMe.html",
+        service: "src/service.html",
+        contact: "src/contact.html",
+        learning: "src/Learning.html",
+        learningDetail: "src/Learning-Detail.html",
+      },
+    },
+  },
 });
